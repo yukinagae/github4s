85c11b43bb4e4bcff1d0da8bd1aed3bab4ea0656
yukinagae <yuki.nagae1130@gmail.com>
2014-09-19 09:44:12 +0900

    add issue

--- src/main/scala/core/Core.scala
+++ src/main/scala/core/Core.scala
@@ -19,6 +19,7 @@ object Core {
     json.completeOption
   }
 
+  def issue_search(query: String): Option[IssueSummary] = base(repository_search_url(query)).map(_.extract[IssueSummary])
   def repository(owner: String, repo: String): Option[Repository] = base(repository_url(owner, repo)).map(_.extract[Repository])
   def repository_search(query: String): Option[RepositorySummary] = base(repository_search_url(query)).map(_.extract[RepositorySummary])
   def user(user: String): Option[User] = base(user_url(user)).map(_.extract[User])
@@ -28,7 +29,7 @@ object Core {
   def main(args: Array[String]) {
 
     try {
-      val s = repository_search("hoge")
+      val s = issue_search("hoge")
       println(s)
     } finally {
       Http.shutdown()
--- src/main/scala/core/Issue.scala
+++ src/main/scala/core/Issue.scala
@@ -2,4 +2,16 @@ package core
 
 import java.util.Date
 
-case class Issue(number: Int, state: String, title: String, body: String, comments: Int, milestone: Option[Milestone], closed_at: Date, created_at: Date, updated_at: Date)
+case class Issue( //
+  number: Int, //
+  state: String, //
+  title: String, //
+  body: String, //
+  comments: Int, //
+  milestone: Option[Milestone], //
+  closed_at: Date, //
+  created_at: Date, //
+  updated_at: Date //
+  )
+
+case class IssueSummary(total_count: Int, incomplete_results: Boolean, items: List[Repository])
